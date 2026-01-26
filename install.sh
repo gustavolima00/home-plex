3e4250b9de4f2ffb103413e1ad7eeb584a60da3d
Gustavo Marques <gustavomarques.gml@gmail.com>
2026-01-26 11:57:57 -0300

    Creation of separate scripts

--- install.sh
+++ install.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 
-# 1. Configurar Hostname (.local)
-sudo hostnamectl set-hostname inspiron-server
-sudo apt-get update && sudo apt-get install avahi-daemon -y
+./scripts/01-system.sh
+./scripts/02-env.sh
+./scripts/03-preconfig.sh
 
-# 2. Instalar Docker (se não existir)
-if ! [ -x "$(command -v docker)" ]; then
-  curl -fsSL https://get.docker.com | sh
-fi
-
-# 3. Criar estrutura de pastas
-mkdir -p $BASE_DATA_PATH/{downloads,media/{filmes,series}}
-mkdir -p $BASE_CONFIG_PATH/{plex,prowlarr,radarr,sonarr,qbittorrent}
-
-# 4. Subir a Stack
+# 2. Subir o Docker
 docker compose up -d
 
-echo "Aguardando inicialização para configurar APIs..."
-sleep 30
+# 3. Wait for Ready
+./scripts/04-wait.sh
 
-# 5. Aqui você pode rodar comandos curl para injetar as API Keys
-# (Conforme discutimos no passo anterior de automação de API)
+# 4. Finalização
+source .env
+echo "--- Setup Finalizado ---"
+echo "Acesse: http://$(hostname).local:$WEB_PORT"
+echo "Login padrão injetado nos arquivos de config."
